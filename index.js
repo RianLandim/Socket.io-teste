1e1d1b0e9a6a47fd19dcf010029a42d84465b8de
Rian Landim <rianlandim22@gmail.com>
2021-11-01 15:13:09 -0300

    att

--- index.js
+++ index.js
@@ -5,24 +5,32 @@ const app = express()
 const cors = require("cors")
 const socket = require("socket.io")
 
-app.use(express())
 app.use(cors())
 
-
 const server = app.listen(5000, () => console.log("Server is running on port 5000"))
-const io = socket(server,{cors: true, origins:["htpp://localhost:3000"]})
+const io = socket(server,{cors: true, origins:["*"]})
 
-io.on("connection", socket => {
-    socket.emit("me", socket.id)
-    // console.log(socket)
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+	// console.log(socket.id + "\n")
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
 
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("callEnded")
-    })
-    socket.on("callUser", data => {
-        io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-    })
-    socket.on("answerCall", data => {
-        io.to(data.to).emit("callAccepted", data.signal)
-    })
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+		// console.log(data)
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+
+	socket.on('error', function(error) {
+		console.log(error)
+	})	
+
+	socket.on('connect_failed', function(err) {
+		console.log(err)
+	})
 })
